c85437a6b1449d5dbde2d4fcf561f793ab1d2f9b
COURTOIS <courtois.a@hotmail.com>
2023-10-09 17:57:40 +0200

    Final (enfin j'espère)

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,34 +1,22 @@
 
-def translate_word(word)
-  vowel = ["a", "e", "i", "o", "u"]
-  
-
-  if vowel.include?(word[0])
-    return word + "ay"
-
-  elsif word =~ /^(sch|thr|squ)/
-    return word[3..-1] + word[0..2] + "ay"
-
-  elsif word.start_with?("qu")
-    return word[2..-1] + word[0..1] + "ay"
-
-  elsif !vowel.include?(word[0]) && !vowel.include?(word[1]) 
-    return word[2..-1] + word[0..1] + "ay"
-    
-    
-  else
-    return word[1..-1] + word[0] + "ay"
-  end
-end
-
-
 def translate(sentence)
+  vowel = ["a", "e", "i", "o", "u"]
 
-  words = sentence.split(' ')
-  translated_words = words.map { |word| translate_word(word) }
-  translated = translated_words.join(' ')
+  translated_words = sentence.split.map do |word|
+    if vowel.include?(word[0])
+      word + "ay"
+    elsif word =~ /^(sch|thr|squ)/
+      word[3..-1] + word[0..2] + "ay"
+    elsif word.start_with?("qu")
+      word[2..-1] + word[0..1] + "ay"
+    elsif !vowel.include?(word[0]) && !vowel.include?(word[1])
+      word[2..-1] + word[0..1] + "ay"
+    else
+      word[1..-1] + word[0] + "ay"
+    end
+  end
 
-  return translated
+  translated_words.join(' ')
 end
 
 
